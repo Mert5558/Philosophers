ede535d6dd11b0d5b63892b69e7f491d579a6298
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-06-07 14:25:23 +0200

    Add files via upload

--- ft_threads.c
+++ ft_threads.c
@@ -6,7 +6,7 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 12:26:25 by merdal            #+#    #+#             */
-/*   Updated: 2024/05/31 13:02:30 by merdal           ###   ########.fr       */
+/*   Updated: 2024/06/07 14:18:40 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	ft_threads(t_philo *philo, t_program *program)
 {
+	pthread_t	supervisor;
 	int i;
 
 	i = 0;
+	pthread_create(&supervisor, NULL, ft_supervisor, program->philo);
 	while (i < program->philo_num)
 	{
 		pthread_create(&philo[i].thread, NULL, ft_routine, &philo[i]);
-		//printf("nabaysun loo\n");
-		//usleep(100);
 		i++;
 	}
 	i = 0;
@@ -30,4 +30,5 @@ void	ft_threads(t_philo *philo, t_program *program)
 		pthread_join(philo[i].thread, NULL);
 		i++;
 	}
+	pthread_join(supervisor, NULL);
 }
